acf51335b330beea7df98581f1df3c2dff670435
ilkayaktas <ilkayaktas@gmail.com>
2018-06-29 00:16:31 +0300

    Initial Commit

---
+++ FreestyleExperiment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FreestyleExperiment
+//
+//  Created by İlkay Aktaş on 29.06.2018.
+//  Copyright © 2018 İlkay Aktaş. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
